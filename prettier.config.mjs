ddaa45824bd5d01f9e064f0e97b376a828798f32
Elhamullah Hossaini <afghanbahram8@gmail.com>
2025-07-12 01:19:24 +0000

    chore(format): add prettier config

---
+++ prettier.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+const config = {
+  arrowParens: "always",
+  endOfLine: "lf",
+  plugins: [],
+  printWidth: 100,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
+};
+
+export default config;
