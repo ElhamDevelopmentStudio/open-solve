73368a73776e8c3827d7f504bc99e5a048209b5c
Elhamullah Hossaini <afghanbahram8@gmail.com>
2025-11-08 11:00:00 +0430

    feat: update staff trpc router index

--- lib/trpc/router/staff/index.ts
+++ lib/trpc/router/staff/index.ts
@@ -1,8 +1,14 @@
 import { router } from "@/lib/trpc/trpc";
 import { staffProblemsRouter } from "@/lib/trpc/router/staff/problems";
 import { staffJudgeRouter } from "@/lib/trpc/router/staff/judge";
+import { staffDiscussionsRouter } from "@/lib/trpc/router/staff/discussions";
+import { staffEditorialsRouter } from "@/lib/trpc/router/staff/editorials";
+import { staffTrailsRouter } from "@/lib/trpc/router/staff/trails";
 
 export const staffRouter = router({
   problems: staffProblemsRouter,
   judge: staffJudgeRouter,
+  discussions: staffDiscussionsRouter,
+  editorials: staffEditorialsRouter,
+  trails: staffTrailsRouter,
 });
